30ecba4df564c43e09c2e5046635775d8e2cb64a
SamB440 <sam@islandearth.net>
2021-06-18 19:41:00 +0100

    Improve how our mixins work. Add Carpet support.

---
+++ src/main/java/com/convallyria/hugestructureblocks/mixin/structure/packet/ClientUpdateStructureBlockUnlimit.java
@@ -0,0 +1,20 @@
+package com.convallyria.hugestructureblocks.mixin.structure.packet;
+
+import net.minecraft.network.packet.c2s.play.UpdateStructureBlockC2SPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(value = UpdateStructureBlockC2SPacket.class, priority = 999) // Carpet support
+public class ClientUpdateStructureBlockUnlimit {
+
+    @ModifyConstant(method = "<init>(Lnet/minecraft/network/PacketByteBuf;)V", constant = @Constant(intValue = 48), require = 0)
+    public int reinitUpper(int value) {
+        return 512;
+    }
+
+    @ModifyConstant(method = "<init>(Lnet/minecraft/network/PacketByteBuf;)V", constant = @Constant(intValue = -48), require = 0)
+    public int reinitLower(int value) {
+        return -512;
+    }
+}
