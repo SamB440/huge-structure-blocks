ddb8de3f4029af93ec4790f5ed37feede8f58d5f
SamB440 <sam@islandearth.net>
2022-02-20 21:18:22 +0000

    Reindent with 4 spaces

--- src/main/java/com/convallyria/hugestructureblocks/HugeStructureBlocksMod.java
+++ src/main/java/com/convallyria/hugestructureblocks/HugeStructureBlocksMod.java
@@ -6,14 +6,14 @@ import org.apache.logging.log4j.Logger;
 
 public class HugeStructureBlocksMod implements ModInitializer {
 
-	public static final String MODID = "hugestructureblocks";
-	public static final String NAME = "Huge Structure Blocks";
-	public static final String VERSION = "1.0.0";
+    public static final String MODID = "hugestructureblocks";
+    public static final String NAME = "Huge Structure Blocks";
+    public static final String VERSION = "1.0.0";
 
-	public static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Huge Structure Blocks is now making your structure blocks even bigger!");
-	}
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Huge Structure Blocks is now making your structure blocks even bigger!");
+    }
 }
