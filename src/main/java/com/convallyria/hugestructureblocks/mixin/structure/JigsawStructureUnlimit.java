944764b2fc7e5b1faaa01ce5b3f90cfe9e994550
SamB440 <sam@islandearth.net>
2024-05-12 18:06:33 +0100

    Update for 1.20.6

--- src/main/java/com/convallyria/hugestructureblocks/mixin/structure/JigsawStructureUnlimit.java
+++ src/main/java/com/convallyria/hugestructureblocks/mixin/structure/JigsawStructureUnlimit.java
@@ -2,11 +2,11 @@ package com.convallyria.hugestructureblocks.mixin.structure;
 
 import com.convallyria.hugestructureblocks.HugeStructureBlocksMod;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.structure.pool.StructurePool;
 import net.minecraft.structure.pool.alias.StructurePoolAliasBinding;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.dynamic.Codecs;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.gen.heightprovider.HeightProvider;
 import net.minecraft.world.gen.structure.JigsawStructure;
@@ -24,34 +24,25 @@ import static net.minecraft.world.gen.structure.Structure.configCodecBuilder;
 @Mixin(value = JigsawStructure.class, priority = 999)
 public class JigsawStructureUnlimit {
 
-    @Shadow
-    @Final
-    @Mutable
-    // Is this even used?
-    public static final int MAX_SIZE = Integer.MAX_VALUE;
-
     // I tried so many ways to do this without replacing the codec but nothing worked.
     @Shadow
     @Final
     @Mutable
-    public static final Codec<JigsawStructure> CODEC = Codecs.validate(
-        RecordCodecBuilder.mapCodec(
-            instance -> instance.group(
-                    configCodecBuilder(instance),
-                    StructurePool.REGISTRY_CODEC.fieldOf("start_pool").forGetter(structure -> structure.startPool),
-                    Identifier.CODEC.optionalFieldOf("start_jigsaw_name").forGetter(structure -> structure.startJigsawName),
-                    Codec.INT.fieldOf("size").forGetter(structure -> structure.size),
-                    HeightProvider.CODEC.fieldOf("start_height").forGetter(structure -> structure.startHeight),
-                    Codec.BOOL.fieldOf("use_expansion_hack").forGetter(structure -> structure.useExpansionHack),
-                    Heightmap.Type.CODEC.optionalFieldOf("project_start_to_heightmap").forGetter(structure -> structure.projectStartToHeightmap),
-                    Codec.INT.fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter),
-                    Codec.list(StructurePoolAliasBinding.CODEC).optionalFieldOf("pool_aliases", List.of()).forGetter(JigsawStructure::getPoolAliasBindings)
+    public static final MapCodec<JigsawStructure> CODEC = RecordCodecBuilder.<JigsawStructure>mapCodec(
+                    instance -> instance.group(
+                                    configCodecBuilder(instance),
+                                    StructurePool.REGISTRY_CODEC.fieldOf("start_pool").forGetter(structure -> structure.startPool),
+                                    Identifier.CODEC.optionalFieldOf("start_jigsaw_name").forGetter(structure -> structure.startJigsawName),
+                                    Codec.INT.fieldOf("size").forGetter(structure -> structure.size),
+                                    HeightProvider.CODEC.fieldOf("start_height").forGetter(structure -> structure.startHeight),
+                                    Codec.BOOL.fieldOf("use_expansion_hack").forGetter(structure -> structure.useExpansionHack),
+                                    Heightmap.Type.CODEC.optionalFieldOf("project_start_to_heightmap").forGetter(structure -> structure.projectStartToHeightmap),
+                                    Codec.INT.fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter),
+                                    Codec.list(StructurePoolAliasBinding.CODEC).optionalFieldOf("pool_aliases", List.of()).forGetter(JigsawStructure::getPoolAliasBindings)
+                            )
+                            .apply(instance, JigsawStructure::new)
             )
-            .apply(instance, JigsawStructure::new)
-        ),
-        JigsawStructure::validate
-    )
-    .codec();
+            .validate(JigsawStructure::validate);
 
     @ModifyConstant(method = "<init>(Lnet/minecraft/world/gen/structure/Structure$Config;Lnet/minecraft/registry/entry/RegistryEntry;ILnet/minecraft/world/gen/heightprovider/HeightProvider;Z)V", constant = @Constant(intValue = 80), require = 0)
     private static int init1(int value) {
--- src/main/java/com/convallyria/hugestructureblocks/mixin/structure/PlaceCommandUnlimit.java
+++ src/main/java/com/convallyria/hugestructureblocks/mixin/structure/PlaceCommandUnlimit.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(value = PlaceCommand.class, priority = 999)
 public class PlaceCommandUnlimit {
 
-    @ModifyConstant(method = "register", constant = @Constant(intValue = 7), require = 0)
+    @ModifyConstant(method = "register", constant = @Constant(intValue = 20), require = 0)
     private static int changeJigsawDepth(int value) {
         return Integer.MAX_VALUE;
     }
