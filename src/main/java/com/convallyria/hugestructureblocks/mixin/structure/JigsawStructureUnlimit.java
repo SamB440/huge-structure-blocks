2afd586299c71bbec4764161dec7a6899a9c0f90
cputnam-a11y <chrlsvryptnm@gmail.com>
2024-07-17 09:13:11 -0500

    Removed ModifyConstant? I hear that ModifyExpressionValue is better for compat

--- src/main/java/com/convallyria/hugestructureblocks/mixin/structure/JigsawStructureUnlimit.java
+++ src/main/java/com/convallyria/hugestructureblocks/mixin/structure/JigsawStructureUnlimit.java
@@ -1,6 +1,7 @@
 package com.convallyria.hugestructureblocks.mixin.structure;
 
 import com.convallyria.hugestructureblocks.HugeStructureBlocksMod;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.serialization.Codec;
@@ -26,38 +27,28 @@ import java.util.List;
 @Mixin(value = JigsawStructure.class, priority = 999)
 @Debug(export = true)
 public abstract class JigsawStructureUnlimit extends Structure {
-//    @Accessor
-//    protected abstract List<StructurePoolAliasBinding> getPoolAliasBindings();
-//    @Accessor
-//    protected abstract DimensionPadding getDimensionPadding();
-//    @Accessor
-//    protected abstract StructureLiquidSettings getLiquidSettings();
-    // I tried so many ways to do this without replacing the codec but nothing worked.
-//    @Shadow
-//    @Final
-//    @Mutable
-//    public static MapCodec<JigsawStructure> CODEC = RecordCodecBuilder.<JigsawStructure>mapCodec((instance) -> instance.group(configCodecBuilder(instance), StructurePool.REGISTRY_CODEC.fieldOf("start_pool").forGetter((structure) -> structure.startPool), Identifier.CODEC.optionalFieldOf("start_jigsaw_name").forGetter((structure) -> structure.startJigsawName), Codec.INT.fieldOf("size").forGetter((structure) -> structure.size), HeightProvider.CODEC.fieldOf("start_height").forGetter((structure) -> structure.startHeight), Codec.BOOL.fieldOf("use_expansion_hack").forGetter((structure) -> structure.useExpansionHack), Heightmap.Type.CODEC.optionalFieldOf("project_start_to_heightmap").forGetter((structure) -> structure.projectStartToHeightmap), Codec.INT.fieldOf("max_distance_from_center").forGetter((structure) -> structure.maxDistanceFromCenter), Codec.list(StructurePoolAliasBinding.CODEC).optionalFieldOf("pool_aliases", List.of()).forGetter((structure) -> ((JigsawStructureUnlimit) (Object) structure).getPoolAliasBindings()), DimensionPadding.CODEC.optionalFieldOf("dimension_padding", JigsawStructure.DEFAULT_DIMENSION_PADDING).forGetter((structure) -> ((JigsawStructureUnlimit) (Object) structure).getDimensionPadding()), StructureLiquidSettings.codec.optionalFieldOf("liquid_settings", JigsawStructure.DEFAULT_LIQUID_SETTINGS).forGetter((jigsawStructure) -> ((JigsawStructureUnlimit) (Object) jigsawStructure).getLiquidSettings())).apply(instance, JigsawStructure::new)).validate(JigsawStructure::validate);
     @WrapOperation(method = {"method_41662"}, at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/Codec;intRange(II)Lcom/mojang/serialization/Codec;"))
     private static Codec<Integer> x(int minInclusive, int maxInclusive, Operation<Codec<Integer>> original) {
         return Codec.INT;
     }
-    @Shadow @Final private DimensionPadding dimensionPadding;
 
     protected JigsawStructureUnlimit(Config config) {
         super(config);
     }
 
-    @ModifyConstant(method = "<init>(Lnet/minecraft/world/gen/structure/Structure$Config;Lnet/minecraft/registry/entry/RegistryEntry;ILnet/minecraft/world/gen/heightprovider/HeightProvider;Z)V", constant = @Constant(intValue = 80), require = 0)
+//    @ModifyConstant(method = "<init>(Lnet/minecraft/world/gen/structure/Structure$Config;Lnet/minecraft/registry/entry/RegistryEntry;ILnet/minecraft/world/gen/heightprovider/HeightProvider;Z)V", constant = @Constant(intValue = 80), require = 0)
+    @ModifyExpressionValue(method = "<init>(Lnet/minecraft/world/gen/structure/Structure$Config;Lnet/minecraft/registry/entry/RegistryEntry;ILnet/minecraft/world/gen/heightprovider/HeightProvider;Z)V", at = @At(value = "CONSTANT", args = "intValue=80"), require = 0)
     private static int init1(int value) {
         return HugeStructureBlocksMod.NEW_STRUCTURE_SIZE;
     }
 
-    @ModifyConstant(method = "<init>(Lnet/minecraft/world/gen/structure/Structure$Config;Lnet/minecraft/registry/entry/RegistryEntry;ILnet/minecraft/world/gen/heightprovider/HeightProvider;ZLnet/minecraft/world/Heightmap$Type;)V", constant = @Constant(intValue = 80), require = 0)
+//    @ModifyConstant(method = "<init>(Lnet/minecraft/world/gen/structure/Structure$Config;Lnet/minecraft/registry/entry/RegistryEntry;ILnet/minecraft/world/gen/heightprovider/HeightProvider;ZLnet/minecraft/world/Heightmap$Type;)V", constant = @Constant(intValue = 80), require = 0)
+    @ModifyExpressionValue(method = "<init>(Lnet/minecraft/world/gen/structure/Structure$Config;Lnet/minecraft/registry/entry/RegistryEntry;ILnet/minecraft/world/gen/heightprovider/HeightProvider;ZLnet/minecraft/world/Heightmap$Type;)V", at = @At(value = "CONSTANT", args = "intValue=80"), require = 0)
     private static int init2(int value) {
         return HugeStructureBlocksMod.NEW_STRUCTURE_SIZE;
     }
-
-    @ModifyConstant(method = "validate", constant = @Constant(intValue = 128), require = 0)
+//    @ModifyConstant(method = "validate", constant = @Constant(intValue = 128), require = 0)
+    @ModifyExpressionValue(method = "validate", at = @At(value = "CONSTANT", args = "intValue=128"), require = 0)
     private static int maxDistanceFromCenter(int value) {
         return HugeStructureBlocksMod.NEW_STRUCTURE_SIZE;
     }
