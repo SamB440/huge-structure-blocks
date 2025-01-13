1a13b6dcae907d4029770b16e9e364a2eef11ab6
SamB440 <sam@islandearth.net>
2025-01-13 20:50:33 +0000

    Add a creative tab for structure items

--- fabric/src/main/java/com/convallyria/hugestructureblocks/fabric/HSBFabric.java
+++ fabric/src/main/java/com/convallyria/hugestructureblocks/fabric/HSBFabric.java
@@ -2,8 +2,24 @@ package com.convallyria.hugestructureblocks.fabric;
 
 import com.convallyria.hugestructureblocks.HugeStructureBlocksMod;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 public final class HSBFabric implements ModInitializer {
+
+    public static final RegistryKey<ItemGroup> STRUCTURE_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(HugeStructureBlocksMod.MOD_ID, "structure_items"));
+    public static final ItemGroup STRUCTURE_ITEM_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(Items.STRUCTURE_BLOCK))
+            .displayName(Text.translatable("itemGroup.hugestructureblocks.structure_items"))
+            .build();
 
     @Override
     public void onInitialize() {
@@ -13,5 +29,15 @@ public final class HSBFabric implements ModInitializer {
 
         // Run our common setup.
         HugeStructureBlocksMod.init();
+
+        // Register the group.
+        Registry.register(Registries.ITEM_GROUP, STRUCTURE_ITEM_GROUP_KEY, STRUCTURE_ITEM_GROUP);
+
+        // Register items to the custom item group.
+        ItemGroupEvents.modifyEntriesEvent(STRUCTURE_ITEM_GROUP_KEY).register(itemGroup -> {
+            itemGroup.add(Items.STRUCTURE_BLOCK);
+            itemGroup.add(Items.JIGSAW);
+            itemGroup.add(Items.STRUCTURE_VOID);
+        });
     }
 }
