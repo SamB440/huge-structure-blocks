c52ea7fa43e0addfe905f167771a52fca7aa6eb1
SamB440 <sam@islandearth.net>
2025-03-12 16:43:27 +0000

    Fix datagen terrain adaptation (fixes #5)

--- common/src/main/java/com/convallyria/hugestructureblocks/mixin/structure/JigsawStructureUnlimit.java
+++ common/src/main/java/com/convallyria/hugestructureblocks/mixin/structure/JigsawStructureUnlimit.java
@@ -14,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(value = JigsawStructure.class, priority = 999)
 @Debug(export = true)
 public abstract class JigsawStructureUnlimit extends Structure {
+
     @WrapOperation(method = {"method_41662"}, at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/Codec;intRange(II)Lcom/mojang/serialization/Codec;"))
     private static Codec<Integer> x(int minInclusive, int maxInclusive, Operation<Codec<Integer>> original) {
         return Codec.INT;
@@ -26,14 +27,15 @@ public abstract class JigsawStructureUnlimit extends Structure {
 //    @ModifyConstant(method = "<init>(Lnet/minecraft/world/gen/structure/Structure$Config;Lnet/minecraft/registry/entry/RegistryEntry;ILnet/minecraft/world/gen/heightprovider/HeightProvider;Z)V", constant = @Constant(intValue = 80), require = 0)
     @ModifyExpressionValue(method = "<init>(Lnet/minecraft/world/gen/structure/Structure$Config;Lnet/minecraft/registry/entry/RegistryEntry;ILnet/minecraft/world/gen/heightprovider/HeightProvider;Z)V", at = @At(value = "CONSTANT", args = "intValue=80"), require = 0)
     private static int init1(int value) {
-        return HugeStructureBlocksMod.NEW_STRUCTURE_SIZE;
+        return HugeStructureBlocksMod.NEW_STRUCTURE_SIZE - 12;
     }
 
 //    @ModifyConstant(method = "<init>(Lnet/minecraft/world/gen/structure/Structure$Config;Lnet/minecraft/registry/entry/RegistryEntry;ILnet/minecraft/world/gen/heightprovider/HeightProvider;ZLnet/minecraft/world/Heightmap$Type;)V", constant = @Constant(intValue = 80), require = 0)
     @ModifyExpressionValue(method = "<init>(Lnet/minecraft/world/gen/structure/Structure$Config;Lnet/minecraft/registry/entry/RegistryEntry;ILnet/minecraft/world/gen/heightprovider/HeightProvider;ZLnet/minecraft/world/Heightmap$Type;)V", at = @At(value = "CONSTANT", args = "intValue=80"), require = 0)
     private static int init2(int value) {
-        return HugeStructureBlocksMod.NEW_STRUCTURE_SIZE;
+        return HugeStructureBlocksMod.NEW_STRUCTURE_SIZE - 12;
     }
+
 //    @ModifyConstant(method = "validate", constant = @Constant(intValue = 128), require = 0)
     @ModifyExpressionValue(method = "validate", at = @At(value = "CONSTANT", args = "intValue=128"), require = 0)
     private static int maxDistanceFromCenter(int value) {
